8c9888e14d89eaf6f2eca56b03cde04ca7cacfe0
Shreyansh Madhav <madhavshreyansh@gmail.com>
2026-02-01 17:05:33 +0530

    feat: implement stable product fetching in Body using useEffect

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ProductItem from './ProductItem';
 import ProductList from './ProductList';
 
 function Body() {
     let productItems = ProductList();
     // console.log(productItems);
+    const [allProductItems, setAllProductItems] = useState(productItems)
+    useEffect(() => {
+        if (productItems && productItems.length) {
+            setAllProductItems(productItems)
+        }
+    }, [productItems])
 
     return (
         <div>
             <div>
 
-                <ProductItem productItems={productItems} />
+                <ProductItem productItems={allProductItems} />
 
             </div>
         </div>
