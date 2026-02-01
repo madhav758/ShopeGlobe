cf78583e6bd8e862c66bf611b0f1d36d50e6f575
Shreyansh Madhav <madhavshreyansh@gmail.com>
2026-02-01 17:05:48 +0530

    feat: set up Redux store and cart slice for global state

---
+++ src/components/utils/cartSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+
+export const cartSlice = createSlice({
+    name: 'cart',
+    initialState: {
+        items: []
+    },
+    reducers: {
+        add: (state, action) => {
+            // Redux Toolkit allows us to write "mutating" logic in reducers. It
+            // doesn't actually mutate the state because it uses the Immer library,
+            // which detects changes to a "draft state" and produces a brand new
+            // immutable state based off those changes
+            state.items.push(action.payload)
+        },
+        remove: (state, action) => {
+            state.items = state.items.filter(item => item.id !== action.payload);
+        },
+        clearCart: (state) => {
+            state.items = [];
+        },
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { add, remove, clearCart } = cartSlice.actions
+
+export default cartSlice.reducer
