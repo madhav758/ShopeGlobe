f67a63e0ebbe428e8bcb192e05d4eaf6fb5ae360
Shreyansh Madhav <madhavshreyansh@gmail.com>
2026-02-03 06:24:33 +0530

    used usedispatch to add the items coming from productItems to the redux store

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,16 +1,24 @@
 import React, { useEffect, useState } from 'react'
 import ProductItem from './ProductItem';
 import ProductList from './ProductList';
+import { useDispatch, useSelector } from 'react-redux'
+import { setProduct } from './utils/productSlice';
 
 function Body() {
     let productItems = ProductList();
     // console.log(productItems);
+    const dispatch = useDispatch()
+
     const [allProductItems, setAllProductItems] = useState(productItems)
     useEffect(() => {
         if (productItems && productItems.length) {
             setAllProductItems(productItems)
+            dispatch(setProduct(productItems))
         }
-    }, [productItems])
+    }, [productItems, dispatch])
+
+
+
 
     return (
         <div>
