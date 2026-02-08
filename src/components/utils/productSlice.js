9a303b8f5e444ffb547090a01b96f75817cc086e
Shreyansh Madhav <madhavshreyansh@gmail.com>
2026-02-09 01:53:22 +0530

    added the filteredproduct and allproduct logic for searching product and displaying product

--- src/components/utils/productSlice.js
+++ src/components/utils/productSlice.js
@@ -6,16 +6,21 @@ export const productSlice = createSlice({
     name: 'product',
     initialState: {
         allProducts: [],
+        filteredProducts: [],
     },
     reducers: {
         setProduct: (state, action) => {
 
             state.allProducts = action.payload
         },
+        setFilteredProduct: (state, action) => {
+
+            state.filteredProducts = action.payload
+        },
     },
 })
 
 // Action creators are generated for each case reducer function
-export const { setProduct } = productSlice.actions
+export const { setProduct, setFilteredProduct } = productSlice.actions
 
 export default productSlice.reducer
